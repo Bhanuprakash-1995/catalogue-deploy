c440dabd063104122cf907567ce8a8ca64146f77
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2024-02-02 13:44:10 +0530

    Jenkins

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,11 +1,23 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "dev-backend-roboshopapp"
+#     key            = "catalogue-dev"
+#     region         = "us-east-1"
+#     dynamodb_table = "dev-lock"
+#   }
+# }
+# provider "aws" {
+#   region = "us-east-1"
+# }
 terraform {
   backend "s3" {
     bucket         = "dev-backend-roboshopapp"
-    key            = "catalogue-dev"
+    key            = "shipping"
     region         = "us-east-1"
     dynamodb_table = "dev-lock"
   }
 }
+
 provider "aws" {
   region = "us-east-1"
 }
