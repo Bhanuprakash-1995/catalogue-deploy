8bcf3cb2b57fbee741b077b10b7edd8131964a54
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2024-02-02 13:39:31 +0530

    Jenkins

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.31.0" # AWS provider version, not terraform version
-    }
-  }
-
   backend "s3" {
     bucket         = "dev-backend-roboshopapp"
     key            = "catalogue-dev"
